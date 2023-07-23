5b34fe5e74903f01ea0d173f3dd80bf95def0317
Tim Batty <tbatty7@gmail.com>
2023-07-22 21:04:49 -0400

    confirmed that I can call the pokemon api direct from HTML Javascript

--- JQuery-testing/script.js
+++ JQuery-testing/script.js
@@ -1,6 +1,10 @@
-$(document).ready(function () {
-    $("button").click(function(){
-        $("p").hide();
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        alert("Text: " + $("#test").text());
     });
-
+    $("#btn2").click(function(){
+        $.get("https://pokeapi.co/api/v2/pokemon/magnemite", function(data, status){
+            alert(status + " - " + JSON.stringify(data));
+        });
+    });
 });
