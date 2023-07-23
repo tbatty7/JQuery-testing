6e25786cf21e95df0e4dd76d036d38467eb64c3a
Tim Batty <tbatty7@gmail.com>
2023-07-22 21:48:07 -0400

    handle errors

--- JQuery-testing/script.js
+++ JQuery-testing/script.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
         alert("Text: " + $("#test").text());
     });
     $("#btn2").click(function(){
-        $.get("https://pokeapi.co/api/v2/pokemon/magnemite", function(data, status){
-            alert(status + " - " + JSON.stringify(data));
+        $.get("https://pokeapi.co/api/v2/pokemon", function(data, status){
+            if (status === "success") {
+                let names = data.results.map(pokemon => pokemon.name);
+                $("#test").text(names.join(' - '));
+            } else {
+                $("#test").text(status);
+            }
+
         });
     });
 });
