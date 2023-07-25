2824533b322a650117b1c9fd7fe4df16a7fd24c0
Tim Batty <tbatty7@gmail.com>
2023-07-24 21:28:02 -0400

    show images next to each pokemon button

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ $(document).ready(function(){
         alert("Text: " + $("#test").text());
     });
     $("#btn2").click(function(){
-        $.get("https://pokeapi.co/api/v2/pokemon", function(data, status){
+        $.get("https://pokeapi.co/api/v2/pokemon", async function(data, status){
             if (status === "success") {
-                const pokemon = data.results;
-                const generatedNameTags = pokemon.map(pokemon => {
+                const pokemonList = data.results;
+                let pokemonImageList = [];
+                for ( pokemon of pokemonList) {
                     const name = pokemon.name;
-                    // $("#"+name).click(function(){
-                    //     $.get("https://pokeapi.co/api/v2/pokemon/"+ name, function(data, status){
-                    //         console.log('inner click called');
-                    //         if (status === "success") {
-                    //             alert(JSON.stringify(data));
-                    //             const spriteUrl = data.sprites.front_default;
-                    //             $("#image-"+name).attr("src", spriteUrl)
-                    //         } else {
-                    //             alert("Call failed with: " + status);
-                    //         }
-                    //     });
-                    // });
+                    const pokemonReference = {name};
+                    await $.get(`https://pokeapi.co/api/v2/pokemon/${name}`, function (data, status) {
+                        if (status === "success") {
+                            const imageUrl = data.sprites.front_default;
+                            console.log(imageUrl);
+                            pokemonReference.imageUrl = imageUrl;
+                            pokemonImageList.push(pokemonReference);
+                        } else {
+                            $("#test").text("single pokemon call failed with --" + status);
+                        }
+                    });
+                }
+
+                const generatedNameTags = pokemonImageList.map(pokemon => {
+                    const name = pokemon.name;
                     return "<p>"+
                         "<button id='"+ name + "'>" + name + "</button>"+
-                        "<img src='empty.png' alt='pokemon image' id='image-" + name + "' width='50' height='50' >"+
+                        "<img src='" + pokemon.imageUrl + "' alt='pokemon image' id='image-" + name + "' width='50' height='50' >"+
                         "</p>";
                 })
                 $("#test").html(generatedNameTags);
